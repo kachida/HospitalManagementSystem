1b75847fcb20264bc87585b865592906f0071d95
KACHIDA <kannappanchidambaram@gmail.com>
2021-07-13 07:00:05 +0530

    Containarize Modules using Docker
    creating container images for the module API's

--- patient-service/src/main/java/com/patientsvc/PatientServiceApplication.java
+++ patient-service/src/main/java/com/patientsvc/PatientServiceApplication.java
@@ -3,8 +3,10 @@ package com.patientsvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PatientServiceApplication {
 
 	public static void main(String[] args) {
